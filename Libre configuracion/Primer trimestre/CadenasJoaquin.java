058e37870fc6705e3a9c7bc4979f1bc756333379
ElTitoJoe <yoabarrancampos@gmail.com>
2023-09-19 12:52:22 +0200

    libre configuracion

---
+++ Libre configuracion/Primer trimestre/CadenasJoaquin.java
@@ -0,0 +1,25 @@
+/*
+Pedir por teclado una cadena
+Mostrar menú:
+Mostrarla al revés
+Contar el nº de vocales
+Contar el nº de consonantes
+Pasar todo a minúscula
+Pasar todo a mayúscula
+Decir si es palíndroma 
+*/
+public class CadenasJoaquin{
+
+    public static void main(String[] args){
+
+        System.out.println("Estas dentro del programa");
+        menu();
+
+    }
+
+    public static void menu(){
+
+        System.out.println("Seleccione una opcion");
+
+    }
+}
