d2d13d901fcca2f9d17bc450798e86488e7642f2
ElTitoJoe <yoabarrancampos@gmail.com>
2023-09-29 12:49:10 +0200

    Actualizacion

--- Libre configuracion/Primer trimestre/Ficha 1 Cadenas/Ej3F1.java
+++ Libre configuracion/Primer trimestre/Ficha 1 Cadenas/Ej3F1.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
-public class Ej3F1 {
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
 
-        // Solicita al usuario que ingrese las dos cadenas
+public class Ej3F1 {
+    public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
         System.out.print("Ingrese la primera cadena: ");
         String cadena1 = teclado.nextLine();
 
         System.out.print("Ingrese la segunda cadena: ");
         String cadena2 = teclado.nextLine();
 
-        // Compara las cadenas utilizando el método compareTo
         int resultado = cadena1.compareTo(cadena2);
 
-        // Comprueba el resultado y muestra el mensaje apropiado
         if (resultado < 0) {
             System.out.println("La primera cadena es menor que la segunda.");
         } else if (resultado > 0) {
@@ -23,5 +21,5 @@ public class Ej3F1 {
         }
 
         teclado.close();
-    } 
+    }
 }
