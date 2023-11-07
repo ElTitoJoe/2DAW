900cc48cd3ebbcab53eb421fb4db5e19bbfb19ed
ElTitoJoe <yoabarrancampos@gmail.com>
2023-11-07 20:50:05 +0100

    Actualizar

--- Desarrollo wen en entorno servidor/Primer Trimestre/Tema 2/R4/Ejercicio4.php
+++ Desarrollo wen en entorno servidor/Primer Trimestre/Tema 2/R4/Ejercicio4.php
@@ -4,7 +4,6 @@ $cadena = "Esto es una cadena un poco más larga";
 $caracter1 = substr($cadena,0,1);
 $caracter3 = substr($cadena,2,1);
 $caracterFinal= substr($cadena,-1);
-echo "G\no\nr\na\n\n";
 echo "$caracter1\n";
 echo "$caracter3\n";
 echo "$caracterFinal\n";
