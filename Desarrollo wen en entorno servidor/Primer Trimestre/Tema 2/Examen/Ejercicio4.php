8017e659e7cd7e8b81f0b812a26f29554456be5e
Joaquín Barranco Campos <125572431+ElTitoJoe@users.noreply.github.com>
2023-11-08 09:05:55 +0100

    Actualizar

--- Desarrollo wen en entorno servidor/Primer Trimestre/Tema 2/Examen/Ejercicio4.php
+++ Desarrollo wen en entorno servidor/Primer Trimestre/Tema 2/Examen/Ejercicio4.php
@@ -2,7 +2,7 @@
 //Dada una frase, escribir la palabra que esté en la posición N.
 //Por ejemplo, $cad = "Hola que tal" y $n=2 debe escribir "que" porque es la segunda palabra de la frase. 
 
-$cad = "Angel 5 y compro jamon";
+$cad = "Hola mi amigo";
 $n = 2; 
 $palabras = explode(" ", $cad);
 if ($n >= 1 && $n <= count($palabras)) {
