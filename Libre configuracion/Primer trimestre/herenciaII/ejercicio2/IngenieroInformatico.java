6bec98f0f442d8d4044bed716eda4b48d90895a3
Joaquín Barranco Campos <125572431+ElTitoJoe@users.noreply.github.com>
2023-12-11 12:29:19 +0100

    Actualizar

---
+++ Libre configuracion/Primer trimestre/herenciaII/ejercicio2/IngenieroInformatico.java
@@ -0,0 +1,7 @@
+package herenciaII.ejercicio2;
+
+public class IngenieroInformatico extends Ingeniero {
+    public void crearPrograma() {
+        System.out.println("El ingeniero informático está creando un programa.");
+    }
+}
