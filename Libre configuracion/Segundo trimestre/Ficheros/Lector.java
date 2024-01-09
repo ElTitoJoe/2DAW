8252dcf7d78743fbef73b5a0be4824a6db12e61f
Joaquín Barranco Campos <125572431+ElTitoJoe@users.noreply.github.com>
2024-01-09 12:52:21 +0100

    Actualizacion

--- Libre configuracion/Segundo trimestre/Ficheros/Lector.java
+++ Libre configuracion/Segundo trimestre/Ficheros/Lector.java
@@ -5,22 +5,14 @@ import java.io.IOException;
 
 public class Lector {
     public static void main(String[] args) {
-        //Usamos el try{}catch para recoger errores
         try {
-           //Creamos un objeto llamado lector que lee lo que hay en text
-            BufferedReader lector = new BufferedReader(new FileReader("\\Users\\Joaquin\\Documents\\GitHub\\2DAW\\Libre configuracion\\Segundo trimestre\\Ficheros\\texto.txt"));
-            // Creamos una cadena llamada linea
+            BufferedReader lector = new BufferedReader(new FileReader("texto.txt"));
             String linea;
-            // Mientras nuestro texto.txt no esté vacío.
             while ((linea = lector.readLine()) != null) {
-              //Imprimimos línea por línea
                 System.out.println(linea);
             }
-            // Cerramos el lector.
             lector.close();
-        //Y capturamos las excepciones
         } catch (IOException e) {
-            // Si hay un error enviamos un mensaje diciendo que hay un error.
             System.out.println("Ha ocurrido un fallo: " + e.getMessage());
         }
     }
