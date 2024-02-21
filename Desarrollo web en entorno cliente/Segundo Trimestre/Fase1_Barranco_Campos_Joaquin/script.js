760096a1fe5354321a83dc4ec97f7264507b9bb3
ElTitoJoe <yoabarrancampos@gmail.com>
2024-02-21 11:07:16 +0100

    Actualizar

---
+++ Desarrollo web en entorno cliente/Segundo Trimestre/Fase1_Barranco_Campos_Joaquin/script.js
@@ -0,0 +1,43 @@
+//Variables
+var resultado = document.getElementById("info");
+var selectRA = document.getElementById("selectRA");
+var datos;
+
+function ajax_get_json()
+{
+    var xmlhttp;
+    if(window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest();
+    }else{ 
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
+            datos = JSON.parse(xmlhttp.responseText);
+            for (var i in datos){
+                for (var j in datos[i]){
+                    var option = document.createElement("option");
+                    option.text = datos[i][j].id + ": " + datos[i][j].textoRA;
+                    option.value = j;
+                    selectRA.add(option);
+                }
+            }
+        }
+    }
+    xmlhttp.open("GET", "datos.json", true);
+    xmlhttp.send();
+}
+
+function mostrarCriterios() {
+    var index = selectRA.value;
+    var criterios = datos["Desarrollo Web en Entorno Cliente"][index].criterios;
+    var peso = datos["Desarrollo Web en Entorno Cliente"][index].peso;
+    resultado.innerHTML = "";
+    for (var i in criterios){
+        resultado.innerHTML += i + ": " + criterios[i] + "<br/>";
+    }
+    resultado.innerHTML += "Peso: " + peso;
+}
+
+window.onload = ajax_get_json;
